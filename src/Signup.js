8ff87e8b11214ce759de2516b84f20793c338299
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2024-01-06 11:37:40 +0530

    Add files via upload

---
+++ src/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Signup = ()=>{
+    return(
+        <div>
+            <h1> Hello World </h1>
+            <h2> My first Hello to the World using React </h2>
+        </div>
+    )
+}
+
+export default Signup;
